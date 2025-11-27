e00a381bf695f22799fd2632a84861af00aa0ab2
신동근 <tlsehdrms124@naver.com>
2025-11-27 17:45:16 +0900

    [BE] feat: Swagger Security 공통 설정 클래스 정의

---
+++ backend/core/common-core/src/main/java/com/followfollowme/nowdoboss/common/support/SwaggerSecurityCommon.java
@@ -0,0 +1,17 @@
+package com.followfollowme.nowdoboss.common.support;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerSecurityCommon {
+
+    @Bean
+    public Components swaggerComponents() {
+        return new Components().addSecuritySchemes("bearerAuth",
+            new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+    }
+}
