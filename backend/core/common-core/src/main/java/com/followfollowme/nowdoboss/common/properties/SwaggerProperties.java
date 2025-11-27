6c5be483a65b8174f822420ea91666841ce112e5
신동근 <tlsehdrms124@naver.com>
2025-11-27 17:42:38 +0900

    [BE] feat: Swagger 프로퍼티 정의

---
+++ backend/core/common-core/src/main/java/com/followfollowme/nowdoboss/common/properties/SwaggerProperties.java
@@ -0,0 +1,10 @@
+package com.followfollowme.nowdoboss.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "swagger")
+public record SwaggerProperties(
+    String serverUrl
+) {
+
+}
