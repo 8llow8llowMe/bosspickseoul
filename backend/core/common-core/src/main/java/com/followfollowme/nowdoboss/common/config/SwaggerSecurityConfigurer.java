5861d7d817194b3f2a104e2b7112a45cb139329a
신동근 <tlsehdrms124@naver.com>
2025-12-05 13:48:55 +0900

    [BE] fix: Swagger Security 관련 Bean 설정 클래스명 변경 (Config -> Configurer)

--- backend/core/common-core/src/main/java/com/followfollowme/nowdoboss/common/config/SwaggerSecurityConfigurer.java
+++ backend/core/common-core/src/main/java/com/followfollowme/nowdoboss/common/config/SwaggerSecurityConfigurer.java
@@ -3,10 +3,8 @@ package com.followfollowme.nowdoboss.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerSecurityConfig {
+public class SwaggerSecurityConfigurer {
 
     @Bean
     public Components swaggerComponents() {
