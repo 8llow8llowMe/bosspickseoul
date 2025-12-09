f21c32d91391057fbbcfb6769a7d6f4814fab36c
신동근 <tlsehdrms124@naver.com>
2025-12-09 16:24:52 +0900

    [BE] feat: JWT 검증 관련 프로퍼티 정의

---
+++ backend/cloud/api-gateway/src/main/java/com/followfollowme/nowdoboss/apigateway/jwt/properties/JwtVerificationProperties.java
@@ -0,0 +1,10 @@
+package com.followfollowme.nowdoboss.apigateway.jwt.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtVerificationProperties(
+    String accessKey
+) {
+
+}
