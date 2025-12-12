b666a853f73c3bdcda53289b91cb8b9c913f15ba
신동근 <tlsehdrms124@naver.com>
2025-12-12 16:44:11 +0900

    [BE] feat: 초기 회원 엔티티 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/member/adapter/out/persistence/entity/MemberEntity.java
@@ -0,0 +1,50 @@
+package com.followfollowme.nowdoboss.domainlayer.member.adapter.out.persistence.entity;
+
+import com.followfollowme.nowdoboss.persistence.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    name = "member",
+    indexes = {
+        @Index(name = "idx_member_email", columnList = "email")
+    })
+public class MemberEntity extends BaseEntity {
+
+    @Id
+    @Comment("회원 아이디")
+    private Long id;
+
+    @Comment("이메일")
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Comment("비밀번호")
+    @Column(length = 80)
+    private String password;
+
+    @Comment("이름")
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Comment("닉네임")
+    @Column(length = 30, nullable = false)
+    private String nickname;
+
+    @Comment("프로필 이미지 URL")
+    private String profileImageUrl;
+}
