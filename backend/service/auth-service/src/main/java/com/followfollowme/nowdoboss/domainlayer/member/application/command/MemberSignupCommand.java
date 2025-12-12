c112bb6adb3828d5079d4fdbab27031e5e32195f
신동근 <tlsehdrms124@naver.com>
2025-12-12 17:35:11 +0900

    [BE] feat: 회원 가입 Command DTO 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/member/application/command/MemberSignupCommand.java
@@ -0,0 +1,22 @@
+package com.followfollowme.nowdoboss.domainlayer.member.application.command;
+
+import com.followfollowme.nowdoboss.domainlayer.member.adapter.in.web.dto.MemberSignupRequest;
+import lombok.Builder;
+
+@Builder
+public record MemberSignupCommand(
+    String email,
+    String password,
+    String name,
+    String nickname
+) {
+
+    public static MemberSignupCommand from(MemberSignupRequest request) {
+        return MemberSignupCommand.builder()
+            .email(request.email())
+            .password(request.password())
+            .name(request.name())
+            .nickname(request.nickname())
+            .build();
+    }
+}
