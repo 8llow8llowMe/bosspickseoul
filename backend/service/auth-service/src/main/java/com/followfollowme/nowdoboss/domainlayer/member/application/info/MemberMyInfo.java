3dcc28b7966338803550f98ee38468464823fdbf
신동근 <tlsehdrms124@naver.com>
2025-12-18 17:25:54 +0900

    [BE] feat: 나의 회원 정보 관련 Info DTO 정의

---
+++ backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/member/application/info/MemberMyInfo.java
@@ -0,0 +1,27 @@
+package com.followfollowme.nowdoboss.domainlayer.member.application.info;
+
+import com.followfollowme.nowdoboss.domainlayer.member.domain.model.Member;
+import com.followfollowme.nowdoboss.security.common.enums.SecurityRole;
+import lombok.Builder;
+
+@Builder
+public record MemberMyInfo(
+    long memberId,
+    String email,
+    String name,
+    String nickname,
+    String profileImageUrl,
+    SecurityRole role
+) {
+
+    public static MemberMyInfo from(Member member) {
+        return MemberMyInfo.builder()
+            .memberId(member.id())
+            .email(member.email())
+            .name(member.name())
+            .nickname(member.nickname())
+            .profileImageUrl(member.profileImageUrl())
+            .role(member.role())
+            .build();
+    }
+}
