959d4e81a0a400a64a3ef9101ea467245378983a
신동근 <tlsehdrms124@naver.com>
2025-12-19 14:58:56 +0900

    [BE] feat: 로그아웃 API 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/auth/adapter/in/web/controller/AuthWebController.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/auth/adapter/in/web/controller/AuthWebController.java
@@ -5,10 +5,14 @@ import com.followfollowme.nowdoboss.domainlayer.auth.adapter.in.web.dto.AuthGene
 import com.followfollowme.nowdoboss.domainlayer.auth.adapter.in.web.dto.AuthGeneralLoginResponse;
 import com.followfollowme.nowdoboss.domainlayer.auth.application.command.AuthGeneralLoginCommand;
 import com.followfollowme.nowdoboss.domainlayer.auth.application.port.in.AuthWebUseCase;
+import com.followfollowme.nowdoboss.security.common.dto.MemberLoginActive;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +34,17 @@ public class AuthWebController {
     public ResponseEntity<Response<AuthGeneralLoginResponse>> loginWithCredentials(@RequestBody AuthGeneralLoginRequest request) {
         AuthGeneralLoginResponse response = authWebUseCase.generalLogin(AuthGeneralLoginCommand.from(request));
         return ResponseEntity.ok().body(Response.success(response));
+    }
+
+    @Operation(
+        summary = "로그아웃",
+        description = "로그인 한 회원을 로그아웃 하는 기능입니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @PostMapping("/logout")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<Void>> logout(@AuthenticationPrincipal MemberLoginActive loginActive) {
+        authWebUseCase.logout(loginActive.id());
+        return ResponseEntity.ok().body(Response.success());
     }
 }
