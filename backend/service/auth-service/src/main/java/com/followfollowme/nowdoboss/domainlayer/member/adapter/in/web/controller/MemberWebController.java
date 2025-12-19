a3e54830fce2d61a156e7782ce8cf034d67c3b48
신동근 <tlsehdrms124@naver.com>
2025-12-19 14:50:31 +0900

    [BE] feat: 나의 회원 정보 조회 API 개발

--- backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/member/adapter/in/web/controller/MemberWebController.java
+++ backend/service/auth-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/member/adapter/in/web/controller/MemberWebController.java
@@ -2,13 +2,19 @@ package com.followfollowme.nowdoboss.domainlayer.member.adapter.in.web.controlle
 
 import com.followfollowme.nowdoboss.common.dto.Response;
 import com.followfollowme.nowdoboss.domainlayer.member.adapter.in.web.dto.request.MemberGeneralSignupRequest;
+import com.followfollowme.nowdoboss.domainlayer.member.adapter.in.web.dto.response.MemberMyInfoResponse;
 import com.followfollowme.nowdoboss.domainlayer.member.application.command.MemberGeneralSignupCommand;
 import com.followfollowme.nowdoboss.domainlayer.member.application.port.in.MemberWebUseCase;
+import com.followfollowme.nowdoboss.security.common.dto.MemberLoginActive;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +36,17 @@ public class MemberWebController {
     public ResponseEntity<Response<Void>> generalSignup(@Valid @RequestBody MemberGeneralSignupRequest request) {
         memberWebUseCase.generalSignup(MemberGeneralSignupCommand.from(request));
         return ResponseEntity.ok().body(Response.success());
+    }
+
+    @Operation(
+        summary = "나의 회원 정보 조회",
+        description = "로그인한 나의 회원 정보를 조회하는 기능입니다.",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<MemberMyInfoResponse>> getMyInfo(@AuthenticationPrincipal MemberLoginActive loginActive) {
+        MemberMyInfoResponse response = memberWebUseCase.getMyInfo(loginActive.id());
+        return ResponseEntity.ok().body(Response.success(response));
     }
 }
