d754fc229e62750495b54ec37f1b9b6d3028fc59
신동근 <tlsehdrms124@naver.com>
2025-12-22 16:00:19 +0900

    [BE] feat: 서비스 타입 enum값 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/domain/enums/ServiceType.java
@@ -0,0 +1,18 @@
+package com.followfollowme.nowdoboss.domainlayer.commercial.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ServiceType {
+
+    RESTAURANT("음식점"),
+    ACADEMY("교육"),
+    LEISURE("레저/오락"),
+    SERVICE("서비스"),
+    RETAIL("도소매업"),
+    HOUSEHOLDS("생활용품");
+
+    private final String description;
+}
