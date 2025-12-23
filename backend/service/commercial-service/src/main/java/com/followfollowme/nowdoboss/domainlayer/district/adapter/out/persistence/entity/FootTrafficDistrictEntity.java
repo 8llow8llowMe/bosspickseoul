a914d07bd809d48cddcbc80d6a96e327dd24b3ef
신동근 <tlsehdrms124@naver.com>
2025-12-23 13:52:01 +0900

    [BE] fix: 유동인구_자치구 엔티티 관련 컬럼 수정 (not null 허용)

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/entity/FootTrafficDistrictEntity.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/entity/FootTrafficDistrictEntity.java
@@ -45,74 +45,90 @@ public class FootTrafficDistrictEntity {
     private String districtCodeName;
 
     @Comment("총 유동인구 수")
+    @Column(nullable = false)
     private Long totalFootTraffic;
 
     @Comment("남성 유동인구 수")
+    @Column(nullable = false)
     private Long maleFootTraffic;
 
     @Comment("여성 유동인구 수")
+    @Column(nullable = false)
     private Long femaleFootTraffic;
 
     @Comment("연령대 10 유동인구 수")
+    @Column(nullable = false)
     private Long teenFootTraffic;
 
     @Comment("연령대 20 유동인구 수")
+    @Column(nullable = false)
     private Long twentyFootTraffic;
 
     @Comment("연령대 30 유동인구 수")
+    @Column(nullable = false)
     private Long thirtyFootTraffic;
 
     @Comment("연령대 40 유동인구 수")
+    @Column(nullable = false)
     private Long fortyFootTraffic;
 
     @Comment("연령대 50 유동인구 수")
+    @Column(nullable = false)
     private Long fiftyFootTraffic;
 
     @Comment("연령대 60 유동인구 수")
+    @Column(nullable = false)
     private Long sixtyFootTraffic;
 
     @Comment("시간대 00 ~ 06 유동인구 수")
-    @Column(name = "foot_traffic_00")
+    @Column(name = "foot_traffic_00", nullable = false)
     private Long footTraffic00;
 
     @Comment("시간대 06 ~ 11 유동인구 수")
-    @Column(name = "foot_traffic_06")
+    @Column(name = "foot_traffic_06", nullable = false)
     private Long footTraffic06;
 
     @Comment("시간대 11 ~ 14 유동인구 수")
-    @Column(name = "foot_traffic_11")
+    @Column(name = "foot_traffic_11", nullable = false)
     private Long footTraffic11;
 
     @Comment("시간대 14 ~ 17 유동인구 수")
-    @Column(name = "foot_traffic_14")
+    @Column(name = "foot_traffic_14", nullable = false)
     private Long footTraffic14;
 
     @Comment("시간대 17 ~ 21 유동인구 수")
-    @Column(name = "foot_traffic_17")
+    @Column(name = "foot_traffic_17", nullable = false)
     private Long footTraffic17;
 
     @Comment("시간대 21 ~ 24 유동인구 수")
-    @Column(name = "foot_traffic_21")
+    @Column(name = "foot_traffic_21", nullable = false)
     private Long footTraffic21;
 
     @Comment("월요일 유동인구 수")
+    @Column(nullable = false)
     private Long monFootTraffic;
 
     @Comment("화요일 유동인구 수")
+    @Column(nullable = false)
     private Long tueFootTraffic;
 
     @Comment("수요일 유동인구 수")
+    @Column(nullable = false)
     private Long wedFootTraffic;
 
     @Comment("목요일 유동인구 수")
+    @Column(nullable = false)
     private Long thuFootTraffic;
 
     @Comment("금요일 유동인구 수")
+    @Column(nullable = false)
     private Long friFootTraffic;
 
     @Comment("토요일 유동인구 수")
+    @Column(nullable = false)
     private Long satFootTraffic;
 
     @Comment("일요일 유동인구 수")
+    @Column(nullable = false)
     private Long sunFootTraffic;
 }
