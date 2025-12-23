242193ed0b4a9aa70a7b9a44dc46236af23669c5
신동근 <tlsehdrms124@naver.com>
2025-12-23 13:56:42 +0900

    [BE] feat: 소득소비_자치구 도메인 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/domain/model/IncomeDistrict.java
@@ -0,0 +1,14 @@
+package com.followfollowme.nowdoboss.domainlayer.district.domain.model;
+
+import lombok.Builder;
+
+@Builder
+public record IncomeDistrict(
+    long id,
+    String periodCode,
+    String districtCode,
+    String districtCodeName,
+    long totalPrice
+) {
+
+}
