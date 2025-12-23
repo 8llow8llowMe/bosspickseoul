42b472d19b9a4e2f61c274eb9f3013b18b47f13c
신동근 <tlsehdrms124@naver.com>
2025-12-23 10:47:30 +0900

    [BE] feat: 점포 행정동 관련 Repository 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/administration/adapter/out/persistence/repository/StoreAdministrationRepository.java
@@ -0,0 +1,8 @@
+package com.followfollowme.nowdoboss.domainlayer.administration.adapter.out.persistence.repository;
+
+import com.followfollowme.nowdoboss.domainlayer.administration.adapter.out.persistence.entity.StoreAdministrationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreAdministrationRepository extends JpaRepository<StoreAdministrationEntity, Long> {
+
+}
