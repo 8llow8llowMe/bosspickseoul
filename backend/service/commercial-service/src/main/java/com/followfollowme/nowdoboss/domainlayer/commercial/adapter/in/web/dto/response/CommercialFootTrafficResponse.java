f72bf7c507e358ce1f2711dfcc6cb0433968cdd4
신동근 <tlsehdrms124@naver.com>
2025-12-24 16:50:13 +0900

    [BE] feat: 상권의 유동 인구 정보 조회 응답 DTO 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/dto/response/CommercialFootTrafficResponse.java
@@ -0,0 +1,27 @@
+package com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response;
+
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeGenderPercentFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeGroupFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialDayOfWeekFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialTimeSlotFootTrafficItem;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "상권의 유동 인구 정보 조회 응답 DTO")
+public record CommercialFootTrafficResponse(
+
+    @Schema(description = "시간대별 유동 인구 정보")
+    CommercialTimeSlotFootTrafficItem timeSlotFootTraffic,
+
+    @Schema(description = "요일별 유동 인구 정보")
+    CommercialDayOfWeekFootTrafficItem dayOfWeekFootTraffic,
+
+    @Schema(description = "연령대별 유동 인구 정보")
+    CommercialAgeGroupFootTrafficItem ageGroupFootTraffic,
+
+    @Schema(description = "연령대 및 성별 유동 인구 비율 정보")
+    CommercialAgeGenderPercentFootTrafficItem ageGenderPercentFootTraffic
+) {
+
+}
