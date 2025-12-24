27b9d8054b58d6c367b4ab58ede7dc606b672abd
신동근 <tlsehdrms124@naver.com>
2025-12-24 15:28:08 +0900

    [BE] feat: 행정동 코드 관련 프로젝션 정의

---
+++ backend/service/district-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/region/adapter/out/persistence/projection/AdministrationCodeProjection.java
@@ -0,0 +1,12 @@
+package com.followfollowme.nowdoboss.domainlayer.region.adapter.out.persistence.projection;
+
+public interface AdministrationCodeProjection {
+
+    String getDistrictCode();
+
+    String getDistrictCodeName();
+
+    String getAdministrationCode();
+
+    String getAdministrationCodeName();
+}
