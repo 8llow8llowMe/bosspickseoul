8905958ccfcfc428c58e7e67bbce429271d8a360
신동근 <tlsehdrms124@naver.com>
2025-12-24 15:28:56 +0900

    [BE] feat: 상권 코드 관련 프로젝션 정의

---
+++ backend/service/district-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/region/adapter/out/persistence/projection/CommercialCodeProjection.java
@@ -0,0 +1,16 @@
+package com.followfollowme.nowdoboss.domainlayer.region.adapter.out.persistence.projection;
+
+public interface CommercialCodeProjection {
+
+    String getDistrictCode();
+
+    String getDistrictCodeName();
+
+    String getAdministrationCode();
+
+    String getAdministrationCodeName();
+
+    String getCommercialCode();
+
+    String getCommercialCodeName();
+}
