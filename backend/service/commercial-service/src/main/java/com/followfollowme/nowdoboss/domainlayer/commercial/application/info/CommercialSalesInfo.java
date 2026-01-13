f543dbe9c12eea18197958424ec839e038f79cde
신동근 <tlsehdrms124@naver.com>
2026-01-13 17:48:50 +0900

    [BE] feat: 상권&업종의 매출 정보 Info DTO 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/application/info/CommercialSalesInfo.java
@@ -0,0 +1,28 @@
+package com.followfollowme.nowdoboss.domainlayer.commercial.application.info;
+
+import com.followfollowme.nowdoboss.domainlayer.commercial.domain.model.SalesCommercial;
+import lombok.Builder;
+
+@Builder
+public record CommercialSalesInfo(
+    CommercialTimeSalesInfo timeSales,
+    CommercialDaySalesInfo daySales,
+    CommercialAgeSalesInfo ageSales,
+    CommercialAgeGenderPercentSalesInfo ageGenderPercentSalesPercentSales,
+    CommercialDaySalesCountInfo daySalesCount,
+    CommercialTimeSalesCountInfo timeSalesCount,
+    CommercialGenderSalesCountInfo genderSalesCount
+) {
+
+    public static CommercialSalesInfo from(SalesCommercial salesCommercial) {
+        return CommercialSalesInfo.builder()
+            .timeSales(CommercialTimeSalesInfo.from(salesCommercial))
+            .daySales(CommercialDaySalesInfo.from(salesCommercial))
+            .ageSales(CommercialAgeSalesInfo.from(salesCommercial))
+            .ageGenderPercentSalesPercentSales(CommercialAgeGenderPercentSalesInfo.from(salesCommercial))
+            .daySalesCount(CommercialDaySalesCountInfo.from(salesCommercial))
+            .timeSalesCount(CommercialTimeSalesCountInfo.from(salesCommercial))
+            .genderSalesCount(CommercialGenderSalesCountInfo.from(salesCommercial))
+            .build();
+    }
+}
