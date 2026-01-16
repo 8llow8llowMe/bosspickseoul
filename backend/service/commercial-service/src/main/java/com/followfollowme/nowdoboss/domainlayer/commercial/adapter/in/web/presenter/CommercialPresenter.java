e4ba013841f64ca543285a876b5e94b27b2131de
신동근 <tlsehdrms124@naver.com>
2026-01-16 16:05:27 +0900

    [BE] feat: 상권&업종 매출 정보 관련 Info -> Response DTO로 매핑 관련 부분 추가

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/presenter/CommercialPresenter.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/presenter/CommercialPresenter.java
@@ -1,16 +1,32 @@
 package com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.presenter;
 
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeGenderPercentFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeGenderPercentSalesItem;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeGroupFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialAgeSalesItem;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialDayOfWeekFootTrafficItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialDaySalesCountItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialDaySalesItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialGenderSalesCountItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialTimeSalesCountItem;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialTimeSalesItem;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.item.CommercialTimeSlotFootTrafficItem;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialFootTrafficResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialSalesResponse;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialServiceCategoryResponse;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialAgeGenderPercentFootTrafficInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialAgeGenderPercentSalesInfo;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialAgeGroupFootTrafficInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialAgeSalesInfo;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialDayOfWeekFootTrafficInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialDaySalesCountInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialDaySalesInfo;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialFootTrafficInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialGenderSalesCountInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialSalesInfo;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialServiceCategoryInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialTimeSalesCountInfo;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialTimeSalesInfo;
 import com.followfollowme.nowdoboss.domainlayer.commercial.application.info.CommercialTimeSlotFootTrafficInfo;
 import java.util.List;
 import org.springframework.stereotype.Component;
@@ -18,6 +34,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommercialPresenter {
 
+    // ---------------------------
+    // Service Category
+    // ---------------------------
     public CommercialServiceCategoryResponse toServiceCategoryResponse(CommercialServiceCategoryInfo info) {
         return CommercialServiceCategoryResponse.builder()
             .serviceCode(info.serviceCode())
@@ -33,6 +52,9 @@ public class CommercialPresenter {
             .toList();
     }
 
+    // ---------------------------
+    // FootTraffic
+    // ---------------------------
     public CommercialFootTrafficResponse toFootTrafficResponse(CommercialFootTrafficInfo info) {
         return CommercialFootTrafficResponse.builder()
             .timeSlotFootTraffic(toTimeSlotFootTrafficItem(info.timeSlotFootTraffic()))
@@ -90,6 +112,99 @@ public class CommercialPresenter {
             .femaleFiftyFootTrafficPercent(info.femaleFiftyFootTrafficPercent())
             .maleSixtyFootTrafficPercent(info.maleSixtyFootTrafficPercent())
             .femaleSixtyFootTrafficPercent(info.femaleSixtyFootTrafficPercent())
+            .build();
+    }
+
+    public CommercialSalesResponse toSalesResponse(CommercialSalesInfo info) {
+        return CommercialSalesResponse.builder()
+            .timeSalesItem(toTimeSalesItem(info.timeSales()))
+            .daySalesItem(toDaySalesItem(info.daySales()))
+            .ageSalesItem(toAgeSalesItem(info.ageSales()))
+            .ageGenderPercentSalesItem(toAgeGenderPercentSalesItem(info.ageGenderPercentSales()))
+            .daySalesCountItem(toDaySalesCountItem(info.daySalesCount()))
+            .timeSalesCountItem(toTimeSalesCountItem(info.timeSalesCount()))
+            .genderSalesCountItem(toGenderSalesCountItem(info.genderSalesCount()))
+            .build();
+    }
+
+    private CommercialTimeSalesItem toTimeSalesItem(CommercialTimeSalesInfo info) {
+        return CommercialTimeSalesItem.builder()
+            .sales00(info.sales00())
+            .sales06(info.sales06())
+            .sales11(info.sales11())
+            .sales14(info.sales14())
+            .sales17(info.sales17())
+            .sales21(info.sales21())
+            .build();
+    }
+
+    private CommercialDaySalesItem toDaySalesItem(CommercialDaySalesInfo info) {
+        return CommercialDaySalesItem.builder()
+            .monSales(info.monSales())
+            .tueSales(info.tueSales())
+            .wedSales(info.wedSales())
+            .thuSales(info.thuSales())
+            .friSales(info.friSales())
+            .satSales(info.satSales())
+            .sunSales(info.sunSales())
+            .build();
+    }
+
+    private CommercialAgeSalesItem toAgeSalesItem(CommercialAgeSalesInfo info) {
+        return CommercialAgeSalesItem.builder()
+            .teenSales(info.teenSales())
+            .twentySales(info.twentySales())
+            .thirtySales(info.thirtySales())
+            .fortySales(info.fortySales())
+            .fiftySales(info.fiftySales())
+            .sixtySales(info.sixtySales())
+            .build();
+    }
+
+    private CommercialAgeGenderPercentSalesItem toAgeGenderPercentSalesItem(CommercialAgeGenderPercentSalesInfo info) {
+        return CommercialAgeGenderPercentSalesItem.builder()
+            .maleTeenSalesPercent(info.maleTeenSalesPercent())
+            .femaleTeenSalesPercent(info.femaleTeenSalesPercent())
+            .maleTwentySalesPercent(info.maleTwentySalesPercent())
+            .femaleTwentySalesPercent(info.femaleTwentySalesPercent())
+            .maleThirtySalesPercent(info.maleThirtySalesPercent())
+            .femaleThirtySalesPercent(info.femaleThirtySalesPercent())
+            .maleFortySalesPercent(info.maleFortySalesPercent())
+            .femaleFortySalesPercent(info.femaleFortySalesPercent())
+            .maleFiftySalesPercent(info.maleFiftySalesPercent())
+            .femaleFiftySalesPercent(info.femaleFiftySalesPercent())
+            .maleSixtySalesPercent(info.maleSixtySalesPercent())
+            .femaleSixtySalesPercent(info.femaleSixtySalesPercent())
+            .build();
+    }
+
+    private CommercialDaySalesCountItem toDaySalesCountItem(CommercialDaySalesCountInfo info) {
+        return CommercialDaySalesCountItem.builder()
+            .monSalesCount(info.monSalesCount())
+            .tueSalesCount(info.tueSalesCount())
+            .wedSalesCount(info.wedSalesCount())
+            .thuSalesCount(info.thuSalesCount())
+            .friSalesCount(info.friSalesCount())
+            .satSalesCount(info.satSalesCount())
+            .sunSalesCount(info.sunSalesCount())
+            .build();
+    }
+
+    private CommercialTimeSalesCountItem toTimeSalesCountItem(CommercialTimeSalesCountInfo info) {
+        return CommercialTimeSalesCountItem.builder()
+            .salesCount00(info.salesCount00())
+            .salesCount06(info.salesCount06())
+            .salesCount11(info.salesCount11())
+            .salesCount14(info.salesCount14())
+            .salesCount17(info.salesCount17())
+            .salesCount21(info.salesCount21())
+            .build();
+    }
+
+    private CommercialGenderSalesCountItem toGenderSalesCountItem(CommercialGenderSalesCountInfo info) {
+        return CommercialGenderSalesCountItem.builder()
+            .maleSalesCount(info.maleSalesCount())
+            .femaleSalesCount(info.femaleSalesCount())
             .build();
     }
 }
