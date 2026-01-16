1192f57ffd367fef8b4631f2f207ab833440773e
신동근 <tlsehdrms124@naver.com>
2026-01-16 17:55:39 +0900

    [BE] feat: 기준 년분기 코드 + 상권 코드로 해당 상권의 집객시설 관련 조회 쿼리 기능 정의(Port 및 Adapter)

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/out/persistence/FacilityCommercialRepositoryAdapter.java
@@ -0,0 +1,23 @@
+package com.followfollowme.nowdoboss.domainlayer.commercial.adapter.out.persistence;
+
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.out.persistence.repository.FacilityCommercialRepository;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.mapper.FacilityCommercialMapper;
+import com.followfollowme.nowdoboss.domainlayer.commercial.application.port.out.FacilityCommercialRepositoryPort;
+import com.followfollowme.nowdoboss.domainlayer.commercial.domain.model.FacilityCommercial;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FacilityCommercialRepositoryAdapter implements FacilityCommercialRepositoryPort {
+
+    private final FacilityCommercialRepository facilityCommercialRepository;
+    private final FacilityCommercialMapper facilityCommercialMapper;
+
+    @Override
+    public Optional<FacilityCommercial> findByPeriodCodeAndCommercialCode(String periodCode, String commercialCode) {
+        return facilityCommercialRepository.findByPeriodCodeAndCommercialCode(periodCode, commercialCode)
+            .map(facilityCommercialMapper::toDomainFromEntity);
+    }
+}
