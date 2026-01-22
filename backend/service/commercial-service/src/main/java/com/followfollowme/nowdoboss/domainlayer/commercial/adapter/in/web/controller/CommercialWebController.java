1d34bdfdb86195e90bdd87aebb45fab1dbfdf31a
신동근 <tlsehdrms124@naver.com>
2026-01-22 15:43:51 +0900

    [BE] feat: 해당 상권의 분기별 집객 시설 조회 API 개발

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/controller/CommercialWebController.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/controller/CommercialWebController.java
@@ -1,6 +1,7 @@
 package com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.controller;
 
 import com.followfollowme.nowdoboss.common.dto.Response;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialFacilityResponse;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialFootTrafficResponse;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialSalesResponse;
 import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialServiceCategoryResponse;
@@ -63,5 +64,18 @@ public class CommercialWebController {
     ) {
         CommercialSalesResponse response = commercialWebUseCase.getSalesByPeriodCodeAndCommercialCodeAndServiceCode(periodCode, commercialCode, serviceCode);
         return ResponseEntity.ok().body(Response.success(response));
+    }
+
+    @Operation(
+        summary = "해당 상권의 분기별 집객 시설 조회",
+        description = "주어진 상권코드에 대해 해당 분기의 집객 시설 데이터를 조회합니다. 기준년분기코드가 주어지지 않으면 2023년 3분기의 데이터를 사용합니다."
+    )
+    @GetMapping("/{commercialCode}}/facility")
+    public ResponseEntity<Response<CommercialFacilityResponse>> getFacilityByPeriodAndCommercialCode(
+        @Parameter(description = "상권 코드", required = true, example = "3110008") @PathVariable String commercialCode,
+        @Parameter(description = "기준 년분기 코드 (YYYYQ 형식)", example = "20233") @RequestParam(defaultValue = "20233") String periodCode
+    ) {
+        CommercialFacilityResponse facilityResponse = commercialWebUseCase.getFacilityByPeriodAndCommercialCode(periodCode, commercialCode);
+        return ResponseEntity.ok().body(Response.success(facilityResponse));
     }
 }
