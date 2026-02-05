4ec2d6ce886b06f8e2b2c6502fb7f1933fc88060
신동근 <tlsehdrms124@naver.com>
2026-02-05 23:46:36 +0900

    [BE] fix: 상권 내 가구 월 평균 소득 정보 Item DTO 클래스명 수정 및 필드명 수정

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/dto/item/AverageIncomeItem.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/adapter/in/web/dto/item/AverageIncomeItem.java
@@ -4,14 +4,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-@Schema(description = "상권 내 가구 월 평균 소득 정보 DTO")
-public record CommercialAverageIncomeItem(
+@Schema(description = "상권 내 가구 월 평균 소득 정보")
+public record AverageIncomeItem(
 
     @Schema(description = "월 평균 소득 금액", example = "4200000")
-    long monthAverageIncome,
+    long monthlyAverageIncomeAmount,
 
     @Schema(description = "소득 구간 코드 (1 ~ 10)", example = "5")
-    int incomeSectionCode
+    int incomeBracketCode
 ) {
 
 }
