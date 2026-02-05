d55bdaa6227be3ac03417373bc296414f23f8b0a
신동근 <tlsehdrms124@naver.com>
2026-02-06 01:02:01 +0900

    [BE] refactor: CommercialWebUseCase 인터페이스 코드 스타일 정리

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/application/port/in/CommercialWebUseCase.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/commercial/application/port/in/CommercialWebUseCase.java
@@ -1,24 +1,24 @@
 package com.followfollowme.nowdoboss.domainlayer.commercial.application.port.in;
 
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialFacilityResponse;
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialFootTrafficResponse;
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialIncomeResponse;
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialPopulationResponse;
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialSalesResponse;
-import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.CommercialServiceCategoryResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.FacilityResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.FootTrafficResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.IncomeAndExpenseResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.ResidentPopulationResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.SalesResponse;
+import com.followfollowme.nowdoboss.domainlayer.commercial.adapter.in.web.dto.response.ServiceCategoryResponse;
 import java.util.List;
 
 public interface CommercialWebUseCase {
 
-    List<CommercialServiceCategoryResponse> getServiceCategoriesByCommercialCode(String commercialCode);
+    List<ServiceCategoryResponse> getServiceCategoriesByCommercialCode(String commercialCode);
 
-    CommercialFootTrafficResponse getFootTrafficByPeriodCodeAndCommercialCode(String periodCode, String commercialCode);
+    FootTrafficResponse getFootTrafficByPeriodCodeAndCommercialCode(String periodCode, String commercialCode);
 
-    CommercialSalesResponse getSalesByPeriodCodeAndCommercialCodeAndServiceCode(String periodCode, String commercialCode, String serviceCode);
+    SalesResponse getSalesByPeriodCodeAndCommercialCodeAndServiceCode(String periodCode, String commercialCode, String serviceCode);
 
-    CommercialFacilityResponse getFacilityByPeriodAndCommercialCode(String periodCode, String commercialCode);
+    FacilityResponse getFacilityByPeriodAndCommercialCode(String periodCode, String commercialCode);
 
-    CommercialPopulationResponse getPopulationByPeriodAndCommercialCode(String periodCode, String commercialCode);
+    ResidentPopulationResponse getPopulationByPeriodAndCommercialCode(String periodCode, String commercialCode);
 
-    CommercialIncomeResponse getIncomeByPeriodCodeAndCommercialCode(String periodCode, String commercialCode);
+    IncomeAndExpenseResponse getIncomeByPeriodCodeAndCommercialCode(String periodCode, String commercialCode);
 }
