7fbb21fe41b484cac835f74316512489cd5d973e
신동근 <tlsehdrms124@naver.com>
2026-02-12 13:32:11 +0900

    feat: 점포 자치구 관련 Repository 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/StoreDistrictRepository.java
@@ -0,0 +1,8 @@
+package com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository;
+
+import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.entity.StoreDistrictEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreDistrictRepository extends JpaRepository<StoreDistrictEntity, Long> {
+
+}
