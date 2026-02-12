81062d66f4f978397aefee972fa078bbcca76b0f
신동근 <tlsehdrms124@naver.com>
2026-02-12 16:46:59 +0900

    [BE] feat: 자치구 관련 Repository 정의

--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/FootTrafficDistrictRepository.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/FootTrafficDistrictRepository.java
@@ -1,8 +1,11 @@
 package com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository;
 
 import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.entity.FootTrafficDistrictEntity;
+import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository.custom.FootTrafficDistrictCustomRepository;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FootTrafficDistrictRepository extends JpaRepository<FootTrafficDistrictEntity, Long> {
-    
+public interface FootTrafficDistrictRepository extends JpaRepository<FootTrafficDistrictEntity, Long>, FootTrafficDistrictCustomRepository {
+
+    Optional<FootTrafficDistrictEntity> findByPeriodCodeAndDistrictCode(String periodCode, String districtCode);
 }
--- backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/SalesDistrictRepository.java
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/SalesDistrictRepository.java
@@ -1,8 +1,11 @@
 package com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository;
 
 import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.entity.SalesDistrictEntity;
+import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository.custom.SalesDistrictCustomRepository;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalesDistrictRepository extends JpaRepository<SalesDistrictEntity, Long> {
+public interface SalesDistrictRepository extends JpaRepository<SalesDistrictEntity, Long>, SalesDistrictCustomRepository {
 
+    Optional<SalesDistrictEntity> findByPeriodCodeAndDistrictCodeAndServiceCode(String periodCode, String districtCode, String serviceCode);
 }
