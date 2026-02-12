3455dbc4448687e27d7412988b4dea49df532cb8
신동근 <tlsehdrms124@naver.com>
2026-02-12 13:31:03 +0900

    [BE] feat: 소득 소비 자치구 관련 Repository 정의

---
+++ backend/service/commercial-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/district/adapter/out/persistence/repository/IncomeDistrictRepository.java
@@ -0,0 +1,8 @@
+package com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.repository;
+
+import com.followfollowme.nowdoboss.domainlayer.district.adapter.out.persistence.entity.IncomeDistrictEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IncomeDistrictRepository extends JpaRepository<IncomeDistrictEntity, Long> {
+
+}
