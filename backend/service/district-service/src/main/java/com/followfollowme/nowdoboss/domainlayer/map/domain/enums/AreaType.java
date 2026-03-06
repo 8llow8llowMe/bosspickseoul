e4b6e0a2d85cc10278f4fd2168dfae4dc66fa47e
신동근 <tlsehdrms124@naver.com>
2026-03-06 14:33:21 +0900

    [BE] feat: 지역 타입 관련 ENUM 정의

---
+++ backend/service/district-service/src/main/java/com/followfollowme/nowdoboss/domainlayer/map/domain/enums/AreaType.java
@@ -0,0 +1,14 @@
+package com.followfollowme.nowdoboss.domainlayer.map.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AreaType {
+    DISTRICT("자치구"),
+    ADMINISTRATION("행정동"),
+    COMMERCIAL("상권");
+
+    private final String description;
+}
